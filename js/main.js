1e9ff494f0ffe5a4f8daccedae64ee3f20267858
Ryan Dulac <analyticrye@gmail.com>
2018-07-04 22:34:46 -0400

    improved jquery code for sticky

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 $(document).ready(function() {
 	//bootstrap function
 	$(window).scroll(function(){
-		if ($(window).scrollTop() > 970) {
+		if ($(window).scrollTop() > $(window).height()) {
 			//boostrap 4 changed the class from navbar-fixed-top to fixed-top
 			$('.navbar').addClass('fixed-top');
-		}
-		if ($(window).scrollTop() < 971) {
+		} else {
 			$('.navbar').removeClass('fixed-top');
 		}
 	});
